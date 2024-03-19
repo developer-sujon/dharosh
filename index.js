dec582f9e75ffd738bfc476985e9bdb7e7319e48
Alfaz108 <alfazbinrumon@gmail.com>
2024-03-19 17:47:36 +0600

    add

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-import * as toast from "./src/utils/toast";
-import { _DRemoveEmpty } from "./src/helpers/array-object";
-
-const test = "this is test ";
-
-export { toast, _DRemoveEmpty, test };
+export { _DRemoveEmpty } from "./src/helpers/array-object";
