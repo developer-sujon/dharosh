e2ab98046250f4c9bd2afffb2c8ab0eeea2bc0df
Alfaz108 <alfazbinrumon@gmail.com>
2024-03-19 18:11:06 +0600

    try remove empty

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-import * as toast from "./src/utils/toast";
-import { _DRemoveEmpty } from "./src/helpers/array-object";
+export * as toast from "./src/utils/toast";
+export { _DRemoveEmpty } from "./src/helpers/array-object";
 
-const test = "this is test ";
-
-export default { toast: toast, _DRemoveEmpty, test };
+export const test = "this is test ";
